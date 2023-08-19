263a59783d8d286842e43474d24c146fb56d4fce
CharlieNiner <9shahril.iskandar6@gmail.com>
2023-08-19 21:24:10 +0800

    updated app.py

--- app.py
+++ app.py
@@ -19,7 +19,9 @@ for idx, prayer_label in enumerate(prayer_labels):
             current_day = datetime.datetime.now().strftime('%d-%m-%Y')
 
             new_data = {"Date": current_day, "Prayer": prayer_label, "Time": current_time}
-            df = df.append(new_data, ignore_index=True)
+            # df = df.append(new_data, ignore_index=True)
+            df2 = pd.DataFrame(new_data, index=[0])
+            df = pd.concat([df, df2])
             df.to_csv("data.csv", index=False)
 
             # Display success message
@@ -30,10 +32,10 @@ selected_prayer = st.selectbox("Select Prayer", df["Prayer"].unique())
 filtered_data = df[df["Prayer"] == selected_prayer]
 
 # Convert time strings to datetime objects
-filtered_data["Time"] = pd.to_datetime(filtered_data["Time"], format='%H:%M:%S')
+# filtered_data["Time"] = pd.to_datetime(filtered_data["Time"], format='%H:%M:%S')
 
 # Create a range of time for the y-axis
-y_range = pd.date_range("00:00:00", "23:59:59", freq="15T").time
+# y_range = pd.date_range("00:00:00", "23:59:59", freq="15T").time
 
 # Plot the line chart
 st.write(f"Line Chart for {selected_prayer} Prayer Time")
