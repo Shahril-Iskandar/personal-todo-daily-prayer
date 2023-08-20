f3241c1c3449e9a7a473f90603eac3b6b7d8e52b
CharlieNiner <9shahril.iskandar6@gmail.com>
2023-08-20 13:10:01 +0800

    updated csv and app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import datetime
 import pytz
+import matplotlib.pyplot as plt
+import altair as alt
 
 prayer_labels = ["Subuh", "Zuhur", "Asar", "Maghrib", "Isyak"]
 
@@ -19,8 +21,8 @@ for idx, prayer_label in enumerate(prayer_labels):
     with columns[idx]:
         if st.button(prayer_labels[idx]):
             # Get the current time
-            current_time = datetime.datetime.now(sg_timezone).strftime('%H:%M:%S')
-            current_day = datetime.datetime.now(sg_timezone).strftime('%d-%m-%Y')
+            current_time = datetime.datetime.now(sg_timezone).strftime('%H:%M')
+            current_day = datetime.datetime.now(sg_timezone).strftime('%Y-%m-%d %H:%M')
 
             new_data = {"Date": current_day, "Prayer": prayer_label, "Time": current_time}
             # df = df.append(new_data, ignore_index=True)
@@ -35,16 +37,32 @@ for idx, prayer_label in enumerate(prayer_labels):
 selected_prayer = st.selectbox("Select Prayer", df["Prayer"].unique())
 filtered_data = df[df["Prayer"] == selected_prayer]
 
-# Convert time strings to datetime objects
-# filtered_data["Time"] = pd.to_datetime(filtered_data["Time"], format='%H:%M:%S')
+st.write(filtered_data)
 
-# Create a range of time for the y-axis
-# y_range = pd.date_range("00:00:00", "23:59:59", freq="15T").time
+fig = alt.Chart(filtered_data).mark_line().encode(
+    x=alt.X('yearmonthdate(Date):O').title('Date'),
+    # y=alt.Y('hoursminutes(Date):T', scale=alt.Scale(domain=['2012-01-01T00:00:00', '2012-01-02T00:00:00'])).title('Time'),
+    y=alt.Y('Time')
+).properties(
+    width=800,
+    height=400,
+    title='Prayer time'
+)
 
-# Plot the line chart
-st.write(f"Line Chart for {selected_prayer} Prayer Time")
-st.line_chart(filtered_data.set_index("Date")["Time"])
+st.altair_chart(fig, use_container_width=True)
 
+# # Plot the line chart
+# st.write(f"Line Chart for {selected_prayer} Prayer Time")
+# st.line_chart(filtered_data.set_index("Date")["Time"])
+
+# st.write(f"Matplotlib figure")
+# fig, ax = plt.subplots()
+# ax.plot(filtered_data['Date'], filtered_data['Time'])
+# ax.set_xlabel('Date')
+# ax.set_ylabel('Time')
+# ax.set_yticks(range(25))
+# ax.set_yticklabels([f'{t:02.0f}:00' for t in range(25)])
+# st.pyplot(fig)
 
 st.header("Existing dataset")
 st.write(df)
