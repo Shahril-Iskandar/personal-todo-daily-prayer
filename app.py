a625ed6bc86452458a5e28dfd008cbf2f2022cc9
CharlieNiner <9shahril.iskandar6@gmail.com>
2023-08-20 18:05:29 +0800

    update app.py

--- app.py
+++ app.py
@@ -11,15 +11,16 @@ prayer_labels = ["Subuh", "Zuhur", "Asar", "Maghrib", "Isyak"]
 sg_timezone = pytz.timezone('Asia/Singapore')
 
 st.title('Solat Time Tracker')
-df = pd.read_csv('data.csv')
+# df = pd.read_csv('data.csv')
 
 deta = Deta(st.secrets["DETA_KEY"])
 db = deta.Base("ToDoApp")
 
+st.header("Log Praying:")
+st.write("Click specific prayer to log time.")
+
 col1, col2, col3, col4, col5 = st.columns(5)
-
 columns = [col1, col2, col3, col4, col5]
-
 for idx, prayer_label in enumerate(prayer_labels):
     with columns[idx]:
         if st.button(prayer_labels[idx]):
@@ -27,24 +28,26 @@ for idx, prayer_label in enumerate(prayer_labels):
             current_time = datetime.datetime.now(sg_timezone).strftime('%H:%M')
             current_day = datetime.datetime.now(sg_timezone).strftime('%Y-%m-%d %H:%M')
 
-            new_data = {"Date": current_day, "Prayer": prayer_label, "Time": current_time}
-            df2 = pd.DataFrame(new_data, index=[0])
-            df = pd.concat([df, df2])
-            df.to_csv("data.csv", index=False)
+            # new_data = {"Date": current_day, "Prayer": prayer_label, "Time": current_time}
+            # df2 = pd.DataFrame(new_data, index=[0])
+            # df = pd.concat([df, df2])
+            # df.to_csv("data.csv", index=False)
             
             db.put({"Date": current_day, "Prayer": prayer_label, "Time": current_time})
 
             # Display success message
             st.success(f"{prayer_label} time logged successfully!")    
 
+st.header("View historical praying time")
 # Filter data for a specific prayer (you can customize this)
-selected_prayer = st.selectbox("Select Prayer", df["Prayer"].unique())
-filtered_data = df[df["Prayer"] == selected_prayer]
-
-st.write(filtered_data)
-
 db_content = db.fetch().items
-st.write(db_content)
+db_content = pd.DataFrame(db_content)
+
+selected_prayer = st.selectbox("Select Prayer", db_content["Prayer"].unique())
+filtered_data = db_content[db_content["Prayer"] == selected_prayer]
+
+st.write("Data for selected prayer:")
+st.write(filtered_data.iloc[:,:-1]) # Show all except the key
 
 fig = alt.Chart(filtered_data).mark_line().encode(
     x=alt.X('yearmonthdate(Date):O').title('Date'),
@@ -53,7 +56,7 @@ fig = alt.Chart(filtered_data).mark_line().encode(
 ).properties(
     width=800,
     height=400,
-    title='Prayer time'
+    title='Historical Prayer Time'
 )
 
 st.altair_chart(fig, use_container_width=True)
@@ -62,5 +65,17 @@ st.altair_chart(fig, use_container_width=True)
 # st.write(f"Line Chart for {selected_prayer} Prayer Time")
 # st.line_chart(filtered_data.set_index("Date")["Time"])
 
-st.header("Existing dataset")
-st.write(df)
+st.header("All praying time")
+st.write(db_content.iloc[:,:-1]) # Show all except the key
+
+fig2 = alt.Chart(db_content).mark_line().encode(
+    x=alt.X('yearmonthdate(Date):O').title('Date'),
+    # y=alt.Y('hoursminutes(Date):T', scale=alt.Scale(domain=['2012-01-01T00:00:00', '2012-01-02T00:00:00'])).title('Time'),
+    y=alt.Y('Time'),
+    color='Prayer:N'
+).properties(
+    width=800,
+    height=400,
+    title='All Historical Prayer Time'
+)
+st.altair_chart(fig2, use_container_width=True)
